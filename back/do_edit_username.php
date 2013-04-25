dc5a0bd42f574cc265a48f5f98078663baed69f1
Jonathan Raymund M. Pandi <pandi.jonathan@gmail.com>
2013-04-25 15:05:06 +0800

    This commit references #13
    UPDATED do_edit_username.php, added queries to temporarily update
    mypeeks and mypeekers database tables

--- back/do_edit_username.php
+++ back/do_edit_username.php
@@ -11,7 +11,12 @@
 	
 	$query1="select * from user where username = \"{$username2}\"";  
 	$query2="UPDATE user SET username=\"{$username2}\" WHERE username=\"{$username}\"";
-
+	
+	//temporary query to update change of username in mypeeks and mypeekers database
+	$query3="UPDATE mypeeks SET username=\"{$username2}\" WHERE username=\"{$username}\"";
+	$query4="UPDATE mypeekers SET peekers=\"{$username2}\" WHERE peekers=\"{$username}\"";
+	//------------------------------------------------------------------------------
+	
 	
 	$result = mysql_query($query1,$conn);
 	if(mysql_num_rows($result)){
@@ -21,6 +26,8 @@
 		//updates the session username to new username
 		$_SESSION['username']=$username2;
 		mysql_query($query2,$conn);
+		mysql_query($query3,$conn);
+		mysql_query($query4,$conn);
 		header("Location:../ui/manage_account.php");
 	}
 ?>
