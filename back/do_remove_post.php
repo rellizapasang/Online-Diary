6505985dd6c418ff168eb3f396dfd3c5badca8e9
Jonathan Raymund M. Pandi <pandi.jonathan@gmail.com>
2013-04-29 13:35:32 +0800

    This commit references #18
    Added a query to delete comments in the database when the post is
    deleted.

--- back/do_remove_post.php
+++ back/do_remove_post.php
@@ -3,11 +3,12 @@
 	require_once("connect.php");
 	
 	//fetch data from post
-	$date_posted=$_POST['date_posted'];
-	$deleteQuery = "delete from post where date_posted='{$date_posted}'";
-	echo $deleteQuery;
+	$post_id=$_POST['post_id'];
+	$deleteQuery = "delete from post where post_id={$post_id}";
+	$deleteCommentQuery = "delete from comment where post_id={$post_id}"; //query to delete comment(s) when a post is deleted
 	//execute delete post query	
-	$result=mysql_query($deleteQuery,$conn);
+	mysql_query($deleteQuery,$conn);
+	mysql_query($deleteCommentQuery,$conn);
 	mysql_close($conn);
 	header("Location:../ui/home.php");
 ?>
