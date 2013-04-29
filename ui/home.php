6aaf80d73744ca505f1b78067fb5552d0e7039c7
Jonathan Raymund M. Pandi <pandi.jonathan@gmail.com>
2013-04-29 13:25:34 +0800

    This commit references #19
    Reverted home.php contents to april26 commit for like/unlike post.

--- ui/home.php
+++ ui/home.php
@@ -50,76 +50,70 @@
 				//for viewing posts
 				require_once("../back/connect.php");
 				$username = $_SESSION['username'];
-
-
+				
+				
 /***
 *	RETRIEVE POSTS
-*/			
-			$retrieveQuery = "select * from post where username = '{$username}' AND date(date_posted)=date(now())";				
-			$result=mysql_query($retrieveQuery,$conn);
-			$content= "{$username}'s today's post<br/>";
-			$date_posted=$result['date_posted'];
-			while($row=mysql_fetch_array($result)){
-				if($row['post_type'] === 'text') $content=$content.$row['post_content']."<br/>"; //displays text				
-				if($row['post_type'] === 'quote') $content=$content.$row['post_content']."<br/>"; //displays quote
-				else if($row['post_type'] === 'link') $content=$content.'<a style="margin-left:10px" target="_blank" href="'.$row['post_content'].'">'.$row['post_content'].'</a><br/>'; //displays link
-				else if($row['post_type'] === 'image') $content=$content."<img alt='' src='post_images/".$row['post_content']."' width='150' height='150'></img><br/>"; //displays image
-				$date_posted=$row['date_posted'];
-				$username=$row['username'];
-			}
-			echo $content;
-/***
-*	LIKE/UNLIKE BUTTON
-*/
-			$checkPostStatus = "select * from post where username='{$username}' and date_posted='{$date_posted}'";
-			$result2=mysql_query($checkPostStatus);
-			$row2=mysql_fetch_array($result2);
-			if($row2['status']=='unlike'){
-				echo '<form method="POST" action="../back/do_like_post.php">';
-				echo '<input name="userName" type="hidden" value="'.$username.'"/><br/>';
-				echo '<input name="date_posted" type="hidden" value="'.$date_posted.'">';
-				echo "<input type='submit' value='Like'></input>";
-				echo "</form>";
-			}
-			else if($row2['status']=='like'){
-				echo '<form method="POST" action="../back/do_unlike_post.php">';
-				echo '<input name="userName" type="hidden" value="'.$username.'"/><br/>';
-				echo '<input name="date_posted" type="hidden" value="'.$date_posted.'">';
-				echo "<input type='submit' value='Unlike'></input>";
-				echo "</form>";
-			}	
+*/				
+				$retrieveQuery = "select * from post where username = '{$username}' order by date_posted desc";				
+				$result=mysql_query($retrieveQuery,$conn);
+				while($row=mysql_fetch_array($result)){
+					if($row['post_type'] === 'text') echo $row['username']." : ".$row['post_content']."<br/>"; //displays text
+					else if($row['post_type'] === 'quote') echo $row['username']." : ".$row['post_content']."<br/>"; //displays quote
+					else if($row['post_type'] === 'link') echo $row['username'].'<a style="margin-left:10px" target="_blank" href="'.$row['post_content'].'">'.$row['post_content'].'</a><br/>'; //displays link
+					else echo $row['username']."<img alt='' src='post_images/".$row['post_content']."' width='150' height='150'></img><br/>"; //displays image
+					//LIKE/UNLIKE BUTTON
+					$checkPostStatus = "select * from post where username='{$row['username']}' and post_id='{$row['post_id']}'";
+					$result2=mysql_query($checkPostStatus);
+					while($row2=mysql_fetch_array($result2)){
+						if($row2['status']=='unlike'){
+							echo '<form method="POST" action="../back/do_like_post.php">';
+							echo '<input name="userName" type="hidden" value="'.$row['username'].'"/><br/>';
+							echo '<input name="postId" type="hidden" value="'.$row['post_id'].'">';
+							echo "<input type='submit' value='Like'></input>";
+							echo "</form>";
+						}
+						else if($row2['status']=='like'){
+							echo '<form method="POST" action="../back/do_unlike_post.php">';
+							echo '<input name="userName" type="hidden" value="'.$row['username'].'"/><br/>';
+							echo '<input name="postId" type="hidden" value="'.$row['post_id'].'">';
+							echo "<input type='submit' value='Unlike'></input>";
+							echo "</form>";
+						}
+					}
 /***
 *	REMOVE POST
 */
-				if($username==$_SESSION['username']){
+				if($row['username']==$_SESSION['username']){
 					echo '<form method="POST" onSubmit="return deletePostAlert()" action="../back/do_remove_post.php">';
 					echo "<input id='remove_post_button' type='submit' value='Remove Post'></input>";
-					echo "<input type='hidden' name='date_posted' value=".$date_posted."></input></form>";
+					echo "<input type='hidden' name='post_id' value=".$row['post_id']."></input></form>";
 				}
 /***
 *	RETRIEVE COMMENTS
 */
-				$retrieveComment = "select * from comment where date_posted=date(now()) order by date ASC";
-				$result2=mysql_query($retrieveComment,$conn);
-				while($comment_row=mysql_fetch_array($result2)){
-					echo $comment_row['username'].": ".$comment_row['comment_content'];
-					if($comment_row['username']==$_SESSION['username']){
-						//deletes comment
-						echo "<form method='POST' onSubmit='return deleteCommentAlert()' action='../back/do_delete_comment.php'>";
-						echo "<input id='remove_button' type='submit' value='Remove'></input>";
-						echo "<input type='hidden' name='comment_id' value=".$comment_row['comment_id']."></input></form>";
-						echo "<div class='date'>".$comment_row['date']."</div></div>";
-					}
-					else echo "<div class='date'>".$comment_row['date']."</div></div>";
+					$retrieveComment = "select * from comment where post_id=".$row['post_id']." order by date ASC";
+					$result2=mysql_query($retrieveComment,$conn);
+					while($comment_row=mysql_fetch_array($result2)){
+						echo $comment_row['username'].": ".$comment_row['comment_content'];
+						if($comment_row['username']==$_SESSION['username']){
+							//deletes comment
+							echo "<form method='POST' onSubmit='return deleteCommentAlert()' action='../back/do_delete_comment.php'>";
+							echo "<input id='remove_button' type='submit' value='Remove'></input>";
+							echo "<input type='hidden' name='comment_id' value=".$comment_row['comment_id']."></input></form>";
+							echo "<div class='date'>".$comment_row['date']."</div></div>";
+						}
+						else echo "<div class='date'>".$comment_row['date']."</div></div>";
 					}
 /***
 *	ADD COMMENT
 */
-				echo "<form method='POST' action='../back/do_add_comment.php'>
-						<textarea style='margin-left:10px;margin-bottom:10px;'rows=3 cols=93 placeholder='comment..' size=60 name='comment_box' required='required'></textarea>
-						<input type='hidden' name='date_posted' value=".$date_posted."></input>
-						<input  class='comment_button' type='submit' value='Comment'></input>
-					  </form>";				
+					echo "<form method='POST' action='../back/do_add_comment.php'>
+							<textarea style='margin-left:10px;margin-bottom:10px;' rows=3 cols=93 placeholder='comment..' size=60 name='comment_box' required='required'></textarea>
+							<input type='hidden' name='post_id' value=".$row['post_id']."></input>
+							<input  class='comment_button' type='submit' value='Comment'></input>
+						  </form>";				
+					}
 			?>
 		</div>
 		-Publish as private or public
