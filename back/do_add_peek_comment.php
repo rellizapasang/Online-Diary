3160ac8d056597fc341b26607be3707b511e8c1f
Jonathan Raymund M. Pandi <pandi.jonathan@gmail.com>
2013-04-29 16:31:51 +0800

    This commit references #29
    Added do_add_peek_comment.php for submitting queries for adding comment
    to posts.

---
+++ back/do_add_peek_comment.php
@@ -0,0 +1,25 @@
+
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+	<?php
+	session_start();
+	require_once("connect.php");
+	
+	//fetch data from post
+	
+	$username2=htmlspecialchars($_POST['userName2']);
+	$postId=htmlspecialchars($_POST['postId']);
+	$username=htmlspecialchars($_SESSION['username']);
+	$comment_content=htmlspecialchars($_POST['comment_box']);
+	$insertQuery = "insert into comment(post_id,username,comment_content,date) values('{$postId}',\"{$username}\",\"{$comment_content}\",sysdate())";
+	
+	
+	$result=mysql_query($insertQuery,$conn);
+	mysql_close($conn);
+	header("Location:../ui/peek_post.php?uName={$username2}");
+	?>
+	</body>
+</html>
