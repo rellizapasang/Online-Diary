3b57c1dc0e107e5cd96067280797078e23440598
Jonathan Raymund M. Pandi <pandi.jonathan@gmail.com>
2013-04-29 16:34:09 +0800

    This commit references #31
    Added do_like_peek_post.php and do_unlike_peek_post.php

---
+++ back/do_like_peek_post.php
@@ -0,0 +1,31 @@
+
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+	<?php
+	//do_like_peek_post.php
+	session_start();
+	require_once("connect.php");
+	if(!isset($_SESSION['username'])){
+		header("Location:../index.php");
+	}
+	$username=$_SESSION['username'];
+	$username2=htmlspecialchars($_POST['userName2']);
+	$postId=htmlspecialchars($_POST['postId']);
+	
+	//queries
+	$insertQuery = "insert into like_table (post_id,username) values('{$postId}',\"{$username}\")";
+	$updatePostStatus = "update post set status='like' where username=\"{$username2}\" and post_id='{$postId}'";
+	
+	
+	//submit queries
+	mysql_query($insertQuery,$conn);
+	mysql_query($updatePostStatus,$conn);
+	header("Location:../ui/peek_post.php?uName={$username2}");
+
+?>
+
+	</body>
+</html>
---
+++ back/do_unlike_peek_post.php
@@ -0,0 +1,32 @@
+
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+	
+<?php
+		//do_like_post.php
+		session_start();
+		require_once("connect.php");
+		if(!isset($_SESSION['username'])){
+			header("Location:../index.php");
+		}
+		$username=$_SESSION['username'];
+		$username2=htmlspecialchars($_POST['userName2']);
+		$postId=htmlspecialchars($_POST['postId']);
+		
+		//queries
+		$deleteQuery = "delete from like_table where post_id='{$postId}' and username='{$username}'";
+		$updatePostStatus = "update post set status='unlike' where username=\"{$username2}\" and post_id='{$postId}'";
+		
+		//echo $updatePostStatus;
+		
+		//submit queries
+		mysql_query($deleteQuery,$conn);
+		mysql_query($updatePostStatus,$conn);
+		header("Location:../ui/peek_post.php?uName={$username2}");
+
+	?>
+	</body>
+</html>
