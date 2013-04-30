f32364f1fb4885e81b78c371ac17652321ab4096
Jonathan Raymund M. Pandi <pandi.jonathan@gmail.com>
2013-04-30 11:34:21 +0800

    Update do_remove_post.php
    Add query to delete likes when post is deleted

--- back/do_remove_post.php
+++ back/do_remove_post.php
@@ -6,6 +6,7 @@
 	$post_id=$_POST['post_id'];
 	$deleteQuery = "delete from post where post_id={$post_id}";
 	$deleteCommentQuery = "delete from comment where post_id={$post_id}"; //query to delete comment(s) when a post is deleted
+	$deleteLikeTableEntry = "delete from like_table where post_id={$post_id}"; //query to delete like(s) when a post is deleted
 	//execute delete post query	
 	mysql_query($deleteQuery,$conn);
 	mysql_query($deleteCommentQuery,$conn);
