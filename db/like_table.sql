e9445f2931cf93aa96709f059dc3cd091aff8314
Jonathan Raymund M. Pandi <pandi.jonathan@gmail.com>
2013-05-03 10:15:51 -0700

    Modified database-like_table.sql

--- db/like_table.sql
+++ db/like_table.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.9
+-- version 3.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 28, 2013 at 06:33 AM
--- Server version: 5.5.8
--- PHP Version: 5.3.5
+-- Generation Time: May 03, 2013 at 05:13 PM
+-- Server version: 5.5.24-log
+-- PHP Version: 5.3.13
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -26,7 +27,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 
 CREATE TABLE IF NOT EXISTS `like_table` (
-  `date_posted` date NOT NULL,
+  `post_id` int(11) NOT NULL,
   `username` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -34,6 +35,11 @@ CREATE TABLE IF NOT EXISTS `like_table` (
 -- Dumping data for table `like_table`
 --
 
-INSERT INTO `like_table` (`date_posted`, `username`) VALUES
-('0000-00-00', 'AthanPandi'),
-('2013-04-28', 'rellizapasang');
+INSERT INTO `like_table` (`post_id`, `username`) VALUES
+(0, 'AthanPandi'),
+(0, 'rellizapasang'),
+(0, 'AthanPandi');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
